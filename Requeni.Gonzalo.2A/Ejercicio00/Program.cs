23d7d50742ca6bc70a754ed4545d40d533d32e47
gonzalorqn <36246342+gonzalorqn@users.noreply.github.com>
2018-08-21 12:14:19 -0300

    Add files via upload

---
+++ Requeni.Gonzalo.2A/Ejercicio00/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = Console.ReadLine();
+            int edad = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Hola mundo");
+
+            Console.WriteLine(nombre);
+            Console.WriteLine(edad);
+
+            Console.ReadLine();
+        }
+    }
+}
